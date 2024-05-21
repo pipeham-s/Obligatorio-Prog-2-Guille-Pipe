6cdd34d85fe7ffec3af891cbb0be87c163f4080a
Guillermojuan <guillejuans@gmail.com>
2024-05-21 12:53:01 -0300

    Entrega 1 24/5

--- src/uy/edu/um/prog2/adt/hash/HashNode.java
+++ src/uy/edu/um/prog2/adt/hash/HashNode.java
@@ -3,12 +3,10 @@ package uy.edu.um.prog2.adt.hash;
 public class HashNode <K,V> {
     private K key;
     private V value;
-    //private boolean deleted;
 
     public HashNode(K key, V value) {
         this.key = key;
         this.value = value;
-       // this.deleted = false;
     }
 
     public K getKey() {
@@ -19,11 +17,4 @@ public class HashNode <K,V> {
         return value;
     }
 
-    //public boolean isDeleted() {
-    //    return deleted;
-    //}
-
-    //public void setDeleted(boolean deleted) {
-    //    this.deleted = deleted;
-    //}
 }
