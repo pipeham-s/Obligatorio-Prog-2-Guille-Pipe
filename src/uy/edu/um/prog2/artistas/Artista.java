c356f4cd015831ff61c32cebfe09097865d187e5
Guillermojuan <guillejuans@gmail.com>
2024-05-27 11:34:04 -0300

    clases que faltabam

--- src/uy/edu/um/prog2/artistas/Artista.java
+++ src/uy/edu/um/prog2/artistas/Artista.java
@@ -2,5 +2,6 @@ package uy.edu.um.prog2.artistas;
 
 public class Artista {
     private String nombre;
+    private String apellido;
     // lista de canciones
 }
---
+++ src/uy/edu/um/prog2/estadisticas/Estadisticas.java
@@ -0,0 +1,5 @@
+package uy.edu.um.prog2.estadisticas;
+
+public class Estadisticas {
+    //van las funciones que pide el obligatorio.
+}
