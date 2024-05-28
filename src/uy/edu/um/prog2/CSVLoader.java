012ed54ab445123eaa7cd6d73ece339e4c5091e4
pipeham-s <pipeham03@gmail.com>
2024-05-28 11:17:55 -0300

    Agregamos CSV

--- src/uy/edu/um/prog2/CSVLoader.java
+++ src/uy/edu/um/prog2/CSVLoader.java
@@ -2,6 +2,21 @@ package uy.edu.um.prog2;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import uy.edu.um.prog2.adt.hash.HashTable;
+import uy.edu.um.prog2.artistas.Artista;
+import uy.edu.um.prog2.canciones.Cancion;
+
+import java.io.FileReader;
+import java.io.Reader;
 
 public class CSVLoader {
-}
+    public static void loadCSVData(HashTable<String, Cancion> canciones, HashTable<String, Artista> artistas) {
+        //     long startTime = System.currentTimeMillis();
+
+        try {
+            Reader in = new FileReader("path al archivo csv");
+
+            Iterable<CSVRecord> records = CSVFormat.DEFAULT
+
+
+        } catch
