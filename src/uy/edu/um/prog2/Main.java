1e844058e7fad9487d26ee70cd384d40fe3da302
pipeham-s <pipeham03@gmail.com>
2024-06-03 10:19:40 -0300

    logica pais de top

--- src/uy/edu/um/prog2/Main.java
+++ src/uy/edu/um/prog2/Main.java
@@ -1,6 +1,22 @@
 package uy.edu.um.prog2;
 
+import uy.edu.um.prog2.adt.hash.HashTable;
+import uy.edu.um.prog2.adt.hash.HashTableImpl;
+import uy.edu.um.prog2.artistas.Artista;
+import uy.edu.um.prog2.canciones.Cancion;
+
+import static uy.edu.um.prog2.CSVLoader.loadCSVData;
+
 public class Main {
 
+    public static void main(String[] args) {
+        HashTable<String, Artista> artistas = new HashTableImpl<>(100);
+        HashTable<String, Cancion> canciones = new HashTableImpl<>(100);
 
+        try {
+            loadCSVData(artistas, canciones);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
