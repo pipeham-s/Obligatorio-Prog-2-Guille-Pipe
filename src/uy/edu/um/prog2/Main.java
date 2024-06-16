17d836580d939baca21e429f8349c789bb0a49bb
pipeham-s <pipeham03@gmail.com>
2024-06-16 17:21:13 -0300

    probando mas csv (pruebaCSV2)

--- src/uy/edu/um/prog2/Main.java
+++ src/uy/edu/um/prog2/Main.java
@@ -5,18 +5,21 @@ import uy.edu.um.prog2.adt.hash.HashTableImpl;
 import uy.edu.um.prog2.artistas.Artista;
 import uy.edu.um.prog2.canciones.Cancion;
 
-import static uy.edu.um.prog2.CSVLoader.loadCSVData;
 
 public class Main {
+    private final HashTable<String, Cancion> canciones;
+    private final HashTable<String, Artista> artistas;
+
+    public Main(HashTable<String, Cancion> canciones, HashTable<String, Artista> artistas) {
+
+        this.canciones = canciones;
+        this.artistas = artistas;
+    }
 
     public static void main(String[] args) {
-        HashTable<String, Artista> artistas = new HashTableImpl<>(100);
-        HashTable<String, Cancion> canciones = new HashTableImpl<>(100);
+        HashTable<String, Cancion> canciones = new HashTableImpl<>(1000000);
+        HashTable<String, Artista> artistas = new HashTableImpl<>(1000000);
 
-        try {
-            loadCSVData(artistas, canciones);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
     }
 }
---
+++ src/uy/edu/um/prog2/PruebaCSV2.java
@@ -0,0 +1,65 @@
+package uy.edu.um.prog2;
+
+import uy.edu.um.prog2.adt.hash.HashTable;
+import uy.edu.um.prog2.adt.hash.HashTableImpl;
+import uy.edu.um.prog2.artistas.Artista;
+import uy.edu.um.prog2.canciones.Cancion;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class PruebaCSV2 {
+    public void CSVReader() {
+        String record;
+        HashTable<String, Cancion> canciones = new HashTableImpl<>(1000000);
+        HashTable<String, Artista> artistas = new HashTableImpl<>(1000000);
+
+        try (BufferedReader br = new BufferedReader(new FileReader("archivo.csv"))) {
+            //Lee la primer linea, que no nos importa
+            br.readLine();
+
+            while ((record = br.readLine()) != null) {
+                record = record.substring(1, record.length() - 2);
+
+
+                String[] values = record.split("\",\"");
+
+                Cancion cancion = new Cancion(values[0], values[1]);
+
+                String[] artistasArray = values[2].split(",");
+                for (String artista : artistasArray) {
+                    artista = artista.trim();
+                    if (artistas.get(artista) != null) {
+                        artistas.get(artista).getCanciones().add(cancion);
+                        cancion.getArtists().add(artistas.get(artista));
+                    } else {
+                        Artista nuevoArtista = new Artista(artista);
+                        artistas.put(artista, nuevoArtista);
+                        nuevoArtista.getCanciones().add(cancion);
+                        cancion.getArtists().add(nuevoArtista);
+                    }
+                }
+                String country = values[6];
+                if (country.equals("")) {
+                    country = "Global";
+                }
+                cancion.setCountry(country);
+
+                cancion.setDailyRank(Integer.parseInt(values[3]));
+                cancion.setDailyMovement(Integer.parseInt(values[4]));
+                cancion.setWeeklyMovement(Integer.parseInt(values[5]));
+                cancion.setSnapshotDate(LocalDate.parse(values[7]));
+                cancion.setAlbumName(values[11]);
+                cancion.setAlbumReleaseDate(LocalDate.parse(values[12]));
+                cancion.setTempo(Float.parseFloat(values[23]));
+
+                canciones.put(values[0], cancion);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
